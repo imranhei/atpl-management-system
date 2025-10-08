13d2c809a82a85b191eeeae6f09b624fc2728f4a
imranhei <imranhossainrana.2014@gmail.com>
2025-10-08 09:53:42 +0600

    test main.yml file for upload backend code

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(cors());
+dotenv.config();
+
+app.get('/health', (req, res) => {
+  res.type('text').send('ok!');
+});
+// if you serve a SPA with a catch-all, keep it LAST:
+app.get('/', (req, res) => res.json({ ups: true }));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
